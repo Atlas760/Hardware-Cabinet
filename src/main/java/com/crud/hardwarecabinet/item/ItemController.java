fa86cd1ba93f86dd2d6551d9c0f3d2903378f138
Antonio Pansini <edaadsvc@gmail.com>
2021-07-07 15:05:39 +0200

    Added ItemController (Handling CRUD basic operations)

---
+++ src/main/java/com/crud/hardwarecabinet/item/ItemController.java
@@ -0,0 +1,69 @@
+package com.crud.hardwarecabinet.item;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.*;
+import java.net.URI;
+
+@RestController
+@RequestMapping("api/menu/items")
+public class ItemController {
+    private final ItemService service;
+
+    public ItemController(ItemService service) {
+        this.service = service;
+    }
+
+    //GET controller methods
+    @GetMapping
+    public ResponseEntity<List<Item>> findAll() {
+        List<Item> items = service.findAll();
+        return ResponseEntity.ok().body(items);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> find(@PathVariable("id") Long id) {
+        Optional<Item> item = service.find(id);
+        return ResponseEntity.of(item);
+    }
+
+    //POST definition
+    @PostMapping
+    public ResponseEntity<Item> create(@RequestBody Item item) {
+        Item created = service.create(item);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    //PUT definition
+    @PutMapping("/{id}")
+    public ResponseEntity<Item> update(
+            @PathVariable("id") Long id,
+            @RequestBody Item updatedItem) {
+
+        Optional<Item> updated = service.update(id, updatedItem);
+
+        return updated
+                .map(value -> ResponseEntity.ok().body(value))
+                .orElseGet(() -> {
+                    Item created = service.create(updatedItem);
+                    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(created.getId())
+                            .toUri();
+                    return ResponseEntity.created(location).body(created);
+                });
+    }
+
+    //DELETE definition
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Item> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
